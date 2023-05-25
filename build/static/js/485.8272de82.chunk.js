c81af7de28140055774babc8bde9dbd962aabd1d
Jeimmy Adrian Moreno Hernandez <adrianmorenojh@hotmail.com>
2023-05-25 18:25:42 -0400

    build

---
+++ build/static/js/485.8272de82.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[485],{4485:function(e,t,r){r.r(t),r.d(t,{default:function(){return l}});var a=r(2791),n=r(5790),u=r(8266);function l(){var e=(0,u.H)("popular",""),t=e.loading,r=e.error,l=e.state,p=e.setPage;return a.createElement(a.Fragment,null,a.createElement(n.v,{loading:t,state:l,setPage:p,error:r}))}}}]);
+//# sourceMappingURL=485.8272de82.chunk.js.map
